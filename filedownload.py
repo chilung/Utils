987289173e4c3bc3888b47e6b08f6dcb65af1948
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-09-23 13:24:27 +0800

    Update and rename ss.py to filedownload.py

--- filedownload.py
+++ filedownload.py
@@ -2,4 +2,4 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 
 gdd.download_file_from_google_drive(file_id='1rR0RcLCBICYh9g7Ii79YvQqrQE7LHdRT',
             dest_path='./kaggle.json',
-            unzip=True)
+            unzip=False)
