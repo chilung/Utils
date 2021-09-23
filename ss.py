c613899dc38791da8bb39e95610360511db32ea7
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-09-23 13:23:52 +0800

    Create ss.py

---
+++ ss.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1rR0RcLCBICYh9g7Ii79YvQqrQE7LHdRT',
+            dest_path='./kaggle.json',
+            unzip=True)
